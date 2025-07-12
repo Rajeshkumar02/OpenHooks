25a2dc98d33bfd3baa6658c5e33bcd8beeb29289
Rajeshkumar S <69568501+Rajeshkumar02@users.noreply.github.com>
2025-07-13 00:14:52 +0530

    Dev init package (#9)
    * feat: add CODEOWNERS, CONTRIBUTING.md, and pull request template for improved contribution guidelines
    
    * feat: add README.md and LICENSE files for project documentation and licensing
    
    * chore: format README.md for improved readability and consistency
    
    * fix: update README.md for clarity and consistency in hosted site references
    
    * feat: Add blog pages and components
    
    - Implemented dynamic blog post pages with metadata generation and inline table of contents.
    - Created a homepage for the blog displaying the latest posts.
    - Added a new EnvURL component for handling environment variables and constructing URLs.
    - Introduced meta.json files for hooks and documentation structure.
    - Added a logo SVG file for branding purposes.

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,10 +1,11 @@
-import { createMDX } from 'fumadocs-mdx/next';
+import { createMDX } from "fumadocs-mdx/next";
 
 const withMDX = createMDX();
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  allowedDevOrigins: ["http://localhost:3000", "192.168.236.26:3000"],
 };
 
 export default withMDX(config);
